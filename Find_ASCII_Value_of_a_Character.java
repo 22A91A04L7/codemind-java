5b3097b7e364bba068d2234c62faffaacb6c62d2
22A91A04L7 <119720834+22A91A04L7@users.noreply.github.com>
2023-05-19 23:20:33 +0530

    import java.util.*; public class assii {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         char ch=sc.next().charAt(0);         int as=ch;         System.out.println(as);     } }

---
+++ Find_ASCII_Value_of_a_Character.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class assii
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        char ch=sc.next().charAt(0);
+        int as=ch;
+        System.out.println(as);
+    }
+}
